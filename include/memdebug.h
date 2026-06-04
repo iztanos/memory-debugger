9345dae527d63fafbc04669b7647a6fc26da2ffb
itzTanos29 <tanushnry@gmail.com>
2026-06-04 02:40:14 +0000

    Define memory debugger public API

--- include/memdebug.h
+++ include/memdebug.h
@@ -1,6 +1,8 @@
 #ifndef MEMDEBUG_H
 #define MEMDEBUG_H
 
+#include <stddef.h>
+
 struct allocation_record {
     void *address;
     size_t size;
@@ -16,5 +18,14 @@ void add_record(void *address, size_t size, const char *file, int line);
 struct allocation_record *find_record(void *address);
 void print_records(void);
 void free_record_list(void);
+
+void *my_malloc(size_t size, const char *file, int line);
+void my_free(void *address, const char *file, int line);
+size_t print_leak_report(void);
+
+#ifndef MEMDEBUG_NO_REPLACE
+#define malloc(size) my_malloc((size), __FILE__, __LINE__)
+#define free(address) my_free((address), __FILE__, __LINE__)
+#endif
 
 #endif
