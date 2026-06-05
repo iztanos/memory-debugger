7b3d1271f113ec23dde8c5f656f74abf5f26478c
itzTanos29 <tanushnry@gmail.com>
2026-06-05 05:54:35 +0000

    Implement deterministic malloc failure simulation

--- src/memdebug.c
+++ src/memdebug.c
@@ -4,9 +4,29 @@
 #include <stdlib.h>
 #include "memdebug.h"
 
+static int fail_after_enabled = 0;
+static size_t allocations_until_failure = 0;
+static size_t failed_allocations = 0;
+
 void *my_malloc(size_t size, const char *file, int line)
 {
-    void *ptr = malloc(size);
+    void *ptr;
+
+    if (fail_after_enabled) {
+        if (allocations_until_failure == 0) {
+            failed_allocations++;
+            fprintf(stderr,
+                    "memdebug warning: simulated malloc failure of %zu byte(s) at %s:%d\n",
+                    size,
+                    file,
+                    line);
+            return NULL;
+        }
+
+        allocations_until_failure--;
+    }
+
+    ptr = malloc(size);
 
     if (ptr == NULL) {
         fprintf(stderr, "memdebug error: malloc failed at %s:%d\n", file, line);
@@ -50,4 +70,23 @@ void my_free(void *address, const char *file, int line)
 
     record->freed = 1;
     free(address);
+}
+
+void memdebug_fail_after(size_t successful_allocations)
+{
+    fail_after_enabled = 1;
+    allocations_until_failure = successful_allocations;
+    failed_allocations = 0;
+}
+
+void memdebug_reset_failures(void)
+{
+    fail_after_enabled = 0;
+    allocations_until_failure = 0;
+    failed_allocations = 0;
+}
+
+size_t memdebug_failed_allocations(void)
+{
+    return failed_allocations;
 }
